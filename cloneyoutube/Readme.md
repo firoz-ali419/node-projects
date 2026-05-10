230003200ea1926de91460b995381106f607f931
FirozKhanHuessain <fzkhan9536@gmail.com>
2026-05-10 09:31:49 +0530

    Reinitialize the project setup

--- cloneyoutube/Readme.md
+++
@@ -1,5 +0,0 @@
-# Youtube Clone App
-
-This is a Youtube clone app
-
-[Model Link](https://app.eraser.io/workspace/YtPqZ1VogxGy1jzIDkzj)
--- cloneyoutube/src/index.js
+++ cloneyoutube/src/index.js
@@ -1,1 +1,26 @@
-console.log("Hello World!!!");
+import dotenv from "dotenv"
+import express from "express"
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+
+dotenv.config({
+    path: './.env'
+})
+
+    ; (async () => {
+        try {
+            await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+            console.log("Connecterd");
+            
+        } catch (error) {
+            console.log("Error", error);
+            throw error
+
+        }
+    })()
+
+
+
+console.log("Index file is Called");
+console.log(process.env.MONGODB_URL);
+
